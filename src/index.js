7fa7d13d212716b60f412335621dde6bfdd20307
Yaroslav <yaroslav.kramarchuk@gmail.com>
2023-06-23 22:59:15 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const rootElement = document.getElementById("root");
 createRoot(rootElement).render(
 	<React.StrictMode>
-		<BrowserRouter basename="/test">
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
